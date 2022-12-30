22b0eeff580290254a33aa1caab7293b6ab18c5e
Sergey Evtushenko <evtushru208@gmail.com>
2022-12-30 21:55:25 +0200

    Начало создания проекта на основе паттерна MVC

--- js/functions/clearClasses.js
+++
@@ -1,6 +0,0 @@
-/*метод удаления классов с элемента */
-const clearAllClasses = (arrayFilters) => {
-	arrayFilters.forEach((element) => { 
-		element.classList.remove('active');
-	});
-};
--- js/functions/renderList.js
+++
@@ -1,7 +0,0 @@
-/*метод рисующий карточки */
-const renderList = (arrElements, containerInViewport) => {
-	
-	arrElements.forEach(element => {
-		containerInViewport.insertAdjacentHTML('afterbegin', renderCard(element));
-	});
-}
